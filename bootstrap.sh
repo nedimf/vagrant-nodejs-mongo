3e6f9a6b364cea209c11089c4bf44fbf2739a154
nedimf <24845593+nedimf@users.noreply.github.com>
2018-09-15 20:48:29 +0200

    Change file at installing mongodb-org to mongodb
    Based on this stackoverflow answer:
    https://stackoverflow.com/a/28966356

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,7 +9,7 @@ sudo apt-get install -y npm
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
 echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
 sudo apt-get update
-sudo apt-get install -y mongodb-org
+sudo apt-get install -y mongodb
 
 # Start and enable the Mongo service so that it automatically starts every time you start the machine
 sudo systemctl start mongod.service
